f7007715d4ce7e0eed7ae8e8a82b59004f987f03
Adeeb hussain <adeebhussain117@gamil.com>
2025-08-25 18:07:39 +0530

    updated

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -6,7 +6,7 @@ const mongodb = async () => {
         console.log("MongoDB connected");
     });
 
-    await mongoose.connect(`${process.env.MONGODB_URL}/bg-removal`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`);
 }
 
 export default mongodb;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
-import 'dotenv/config';
+import 'dotenv/config'
 import express from 'express';
 import cors from 'cors';
 import mongoDB from './config/mongodb.js';
-import serverless from 'serverless-http';
 
+const PORT = process.env.PORT || 5000;
 const app = express();
-
-// Connect to MongoDB once
 await mongoDB();
 
 app.use(cors());
@@ -16,12 +14,6 @@ app.get('/', (req, res) => {
   res.send('API Working!');
 });
 
-// In Vercel functions, export a handler. Locally, start a server.
-export const handler = serverless(app);
-
-if (!process.env.VERCEL) {
-  const port = process.env.PORT || 5000;
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
