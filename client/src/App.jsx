9528106d725245d932434d2416b14a0f75fcd13d
Adeeb hussain <adeebhussain117@gamil.com>
2025-09-18 19:34:15 +0530

    add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Result from './pages/Result'
 import Buycredit from './pages/Buycredit'
 import Navbar from './components/Navbar'
