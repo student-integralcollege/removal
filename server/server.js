c52bf29d67e01fe96698a8020ddb8085fd18d5f3
Adeeb hussain <adeebhussain117@gamil.com>
2025-09-18 22:13:55 +0530

    add

--- server/server.js
+++ server/server.js
@@ -9,18 +9,31 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 await mongoDB();
 
+
+const allowedOrigins = [
+  "http://localhost:5173",         // local dev
+  "https://removal-ivb9.vercel.app" // your deployed frontend
+];
+
 app.use(cors({
-  origin: 'https://removal-ivb9.vercel.app', // your frontend domain
-  credentials: true, // if you use cookies or authentication
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
 }));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API Working!');
 });
+
 app.use('/api/user', userRouter);
 app.use('/api/image', imageRouter);
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
